66510ca75734ef06a40dd70ebe944db2245ada24
Gyeongmin Kim <jyysyl6@naver.com>
2023-03-13 23:17:23 +0900

    프로그래머스 레벨 1 x만큼 간격이 있는 n개의 숫자

--- src/Programmers/level1_1.java
+++ src/Programmers/level1_1.java
@@ -1,5 +1,21 @@
 package Programmers;
 
-public class level1_1 {
+import java.util.Arrays;
 
+public class level1_1 {
+	
+	public static void main(String[] args) {
+		// x만큼 간격이 있는 n개의 숫자
+		
+		long x = 2;
+		// 문제에서는 x가 int형이었는데 int로 하면 x + (x*i)를 했을 때 오버플로우가 발생해서 long형으로 변경했다!
+		int n = 5;
+		long[] answer = new long[n];
+		
+		for(int i = 0; i < n; i++) {
+			answer[i] += x + (x*i);
+		}
+		
+		System.out.println(Arrays.toString(answer));
+	}
 }
