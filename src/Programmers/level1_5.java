874433a90f171a5dfa837e6499f60042692d8332
Gyeongmin Kim <jyysyl6@naver.com>
2023-03-23 20:07:50 +0900

    230323 프로그래머스 level1 약수의 개수와 덧셈, 부족한 금액 계산하기

---
+++ src/Programmers/level1_5.java
@@ -0,0 +1,64 @@
+package Programmers;
+
+public class level1_5 {
+	
+	public static void main(String[] args) {
+		
+		// 1. 약수의 개수와 덧셈
+		
+		int left = 13;
+		int right = 17;
+		int result = 0;
+		int count = 0;
+		
+		for(int i = left; i <= right; i++) {
+			count = 0;
+			for(int j = 1; j <= i; j++) {
+				if(i % j == 0) {
+					count++;
+				}
+			}
+
+			if(count % 2 == 0) {
+				result += i;
+			}else {
+				result -= i;
+			}
+		}
+		
+		System.out.println(result);
+		
+		
+		// 2. 부족한 금액 계산하기
+		
+		int price = 3;
+		int money  =20;
+		int count2 = 4;
+		int sub_answer = 0;
+		long answer = 0;
+		
+		for(int i = 0; i <= count2; i++) {
+			sub_answer += (price * i);
+		}
+		System.out.println(sub_answer);
+		answer = sub_answer - money;
+		System.out.println(answer);
+		
+		
+		// 3. 최대공약수와 최소공배수
+		
+		int n = 3, m = 12;
+		int[] answer3 = new int[2];
+		int[] sub_answer3 = {};
+		
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				
+			}
+		}
+ 		
+		
+		
+	}
+
+}
